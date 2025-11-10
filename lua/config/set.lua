e1e759baee6d7dd3c2535c610d76e0c0da6ab38a
Luca Rahm <lucarahm@users.noreply.github.com>
2025-11-10 21:05:49 +0100

    extended with treesitter, lsp and autoformat

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -1,5 +1,5 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 vim.g.have_nerd_font = true
 
@@ -17,7 +17,7 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 -- Sign column on for git
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 
 -- Enable break indent
 vim.opt.breakindent = true
@@ -25,22 +25,19 @@ vim.opt.breakindent = true
 -- Decrease update time
 vim.opt.updatetime = 400
 
-
 -- Split to the right/bottom as default
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
 -- custom chars for spacing etc
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+-- vim.opt.list = true
+-- vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Live preview for substitutions
-vim.opt.inccommand = 'split'
+vim.opt.inccommand = "split"
 
 vim.opt.cursorline = true
 
 vim.opt.scrolloff = 10
 
 vim.opt.linebreak = true
-
-
